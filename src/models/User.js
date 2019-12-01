15331db69ce87a0fa501d4210348f37a07986b94
Pablo Villamizar <pablomvillamizar@gmail.com>
2019-12-01 01:50:39 +0100

    Esquema de Usuario

---
+++ src/models/User.js
@@ -0,0 +1,24 @@
+const { Schema,model } = require("mongoose");
+
+//importar bcrypt
+const bcrypt = require("bcryptjs");
+
+//esquema de usuario
+const userSchema = new Schema({
+	email: String,
+	password: String
+})
+
+//encriptar contraseña
+userSchema.methods.encryptPassword = async (password) => {
+	const salt = await bcrypt.genSalt(10);
+	return bcrypt.hash(password,salt);
+}
+
+//validar contraseña
+userSchema.methods.validatePassword = function(password){
+	return bcrypt.compare(password,this.password);
+}
+
+//exportar esquema
+module.exports = model("User",userSchema);
