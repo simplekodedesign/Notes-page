df17a111938090ba4fed41a899af5c3925c29ebc
Pablo Miguel <53399357+pablom58@users.noreply.github.com>
2020-01-19 20:23:56 +0100

    Login

--- src/app/modules/App.js
+++ src/app/modules/App.js
@@ -4,14 +4,16 @@ import Login from "./views/Login"
 import SignUp from "./views/SignUp"
 import Home from "./views/Home"
 
-const App = () => (
-	<div>
-		<Switch>
-			<Route exact path="/" component={Login}/>
-			<Route path="/signup" component={SignUp}/>
-			<Route path="/home" component={Home}/>
-		</Switch>
-	</div>
-)
+const App = () => {
+	return(
+		<div>
+			<Switch>
+				<Route exact path="/" component={Login}/>
+				<Route path="/signup" component={SignUp}/>
+				<Route path="/home" component={Home}/>
+			</Switch>
+		</div>
+	)
+}
 
 export default App
--- src/app/modules/components/LoginForm.js
+++ src/app/modules/components/LoginForm.js
@@ -1,13 +1,56 @@
-import React from "react"
+import React,{useState} from "react"
 import {Link} from "react-router-dom"
+
+import handlePopup from "./handlePopup" 
 
 const LoginForm = () => {
 
+	const [User,setUser] = useState({
+		email: "",
+		password: ""
+	})
+
+	//controlar los inputs
+	function handleInputChange(e){
+		const {name,value} = e.target
+		setUser(prev => {
+			return({
+				...prev,
+				[name]: value
+			})
+		})
+	}
+
+	//controlar el envio del formulario
+	function handleSubmit(e){
+		e.preventDefault()
+		fetch("/login",{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(User)
+		})
+		.then(resp => resp.json())
+		.then(resp => {
+			if(resp.status != 1){
+				handlePopup(resp.message)
+			}else{
+				window.location = "/home"
+			}
+			setUser({
+				email: "",
+				password: ""
+			})
+		})
+		.catch(err => console.log(err))
+	}
+
 	return (
 		<div className="form_cont">
-			<form autoComplete="off">
-				<input type="text" name="email" placeholder="Email"/>
-				<input type="text" name="password" placeholder="Password"/>
+			<form onSubmit={handleSubmit} autoComplete="off">
+				<input type="text" name="email" placeholder="Email" onChange={handleInputChange} value={User.email}/>
+				<input type="text" name="password" placeholder="Password" onChange={handleInputChange} value={User.password}/>
 				<input type="submit" value="Log In"/>
 			</form>
 			<Link to="/signup">Sign Up</Link>	
