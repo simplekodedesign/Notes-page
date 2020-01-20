1c64b593122812c352c7bfda1da2e2d0793a8266
Pablo Miguel <53399357+pablom58@users.noreply.github.com>
2020-01-20 02:14:14 +0100

    Login

--- src/app/modules/views/Home.js
+++ src/app/modules/views/Home.js
@@ -1,17 +1,18 @@
-import React from "react"
+import React,{useEffect} from "react"
 import Header from "../components/Header"
 import Main from "../components/Main"
 
 
 const Home = (props) => {
 
-	console.log(props)
+	console.log(props.token)
 
 	return (
 		<div>
 			<Header/>
 			<Main/>
-		</div>
+			<p>{props.token}</p>
+		</div>		
 	)
 }
 
